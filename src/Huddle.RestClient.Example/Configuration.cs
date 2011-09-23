769af0a35411bb8988c3d0787b1b8543674f144d
Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
2011-09-23 12:02:49 +0100

    Modified the Readme to include notes on required dependencies and how to get the tests passing

--- src/Huddle.RestClient.Example/Configuration.cs
+++ src/Huddle.RestClient.Example/Configuration.cs
@@ -5,19 +5,19 @@ namespace Huddle.Clients.Example
     public static class Configuration
     {
         /// set this to the ID of the user that you are using for testing.
-        internal const string TestUserId = "123456789";
+        internal const string TestUserId = "13470075";
 
         /// set this to a valid folder ID for the user above.  
         /// NB this is used to hack URIs and is **NOT** the correct approach to use the
         /// Huddle files API.
-        internal const string TestFolderId = "123456789";
+        internal const string TestFolderId = "15549923";
 
         /// set this to the full domain and protocol for your API request.
-        internal const string BaseUrl = "http://api.huddle.net/";
-        internal const string BaseUrlSsl = "https://api.huddle.net/";
+        internal const string BaseUrl = "http://api.huddle.dev/";
+        internal const string BaseUrlSsl = "https://api.huddle.dev/";
 
         /// set this to the OAuth2 access token that you wish to use for testing.
-        internal static readonly string OAuth2Token = "YOU-VALID-TOKEN-HERE";
+        internal static readonly string OAuth2Token = "{'iss':'auth.huddle.dev', 'exp': 1986681600, 'urn:huddle.claims.userid': 13470075}";
 
         internal static readonly string GetTestUserUri = string.Format(BaseUrl + "users/{0}", TestUserId);
         internal static readonly string GetSecureTestUserUri = string.Format(BaseUrlSsl + "users/{0}", TestUserId);
